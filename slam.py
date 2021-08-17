ca95fdeebc6e43d42a2d0374bc1d5803a9419090
Ignat Penshin <61110400+ignatpenshin@users.noreply.github.com>
2021-08-17 17:38:01 +0300

    default ORB realisation with video_crop

--- slam.py
+++ slam.py
@@ -1,8 +1,16 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-cap = cv2.VideoCapture('video.mp4')
-orb = cv2.ORB_create()
+cap = cv.VideoCapture('video.mp4')
+orb = cv.ORB_create()
+
+crop_value = 2
+
+if cap.isOpened():
+    width  = int(cap.get(cv.CAP_PROP_FRAME_WIDTH)//crop_value)
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)//crop_value)
+    fps = cap.get(cv.CAP_PROP_FPS)
+
 
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
@@ -11,19 +19,21 @@ while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
   if ret == True:
+
+    frame = cv.resize(frame, (width, height))
     kp = orb.detect(frame, None)
     kp, des = orb.compute(frame, kp)
-    frame = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
+    frame = cv.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
 
     # Display the resulting frame
-    cv2.imshow('Frame',frame)
+    cv.imshow('Frame',frame)
 
     # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if cv.waitKey(25) & 0xFF == ord('q'):
       break
 
   else: 
     break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
